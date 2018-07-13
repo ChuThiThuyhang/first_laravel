7030be0167ea79fb4095c79675ee6b05d6b0ea29
chi.thi.thuy.hang@framgia.com <chu.thi.thuy.hang@framgia.com>
2018-07-13 16:38:27 +0700

    new commit

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,5 @@
 @extends('master')
 @section('title', 'Contact')
-
 @section('content')
     <div class="container">
         <div class="content">
@@ -8,6 +7,5 @@
             <div class="quote">Our contact page!</div>
         </div>
     </div>
-
 @endsection
 
